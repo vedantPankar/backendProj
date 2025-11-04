0b68a0d664b54925994a8794635abda0f78bc634
Vedant Pankar <vedantpankar2@gmail.com>
2025-11-04 17:15:58 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -10,9 +10,14 @@ app.use(
     credentials: true,
   })
 );
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
