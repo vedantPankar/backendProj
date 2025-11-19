0a7d0306c7c4d267c4c1a49bfb8a7d9dbfb1b040
Vedant Pankar <vedantpankar2@gmail.com>
2025-11-19 21:41:01 +0530

    Fix formatting in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -30,7 +30,7 @@ router.route("/register").post(
 );
 router.route("/login").post(loginUser);
 
-//secured routes
+//secured routes 
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
