a8cc56eedaa94ef5d5d90bb459cd20d2416d3e95
Vedant Pankar <vedantpankar2@gmail.com>
2025-12-04 13:51:10 +0530

    Fix formatting in video.controller.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,8 +1,6 @@
 import { Video } from "../models/video.model";
 import { asyncHandler } from "../utils/asyncHandler";
 
-const uploadVideo = asyncHandler(async () => {
-}
-
+const uploadVideo = asyncHandler(async () => {});
 
 export { uploadVideo };
