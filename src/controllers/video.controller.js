91b9f55ad016d3af31303c9219b443218bb143af
Vedant Pankar <vedantpankar2@gmail.com>
2025-12-05 18:02:15 +0530

    removed unused import fom video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,4 +1,3 @@
-import { upload } from "../middlewares/multer.middleware.js";
 import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
