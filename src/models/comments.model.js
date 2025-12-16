2a6ae5ee08eeb9859102bfa3ff8b8c7c8b7bf9fd
Vedant Pankar <vedantpankar2@gmail.com>
2025-12-16 17:20:43 +0530

    Add comments schema model for MongoDB integration

---
+++ src/models/comments.model.js
@@ -0,0 +1,19 @@
+import { Schema } from "mongoose";
+
+const commentScheme = new Schema(
+  {
+    contant: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
